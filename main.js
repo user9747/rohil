62190a3ebb0ab8c04a4675f5a22d32073d6dda92
user9747 <padinju@gmail.com>
2020-05-13 21:17:36 +0530

    breadcrumbs contacts

--- main.js
+++ main.js
@@ -24,13 +24,34 @@ setInterval(setTheme, 60000);
 function toContact() {
     var about = document.getElementById("about");
     var contact = document.getElementById("contact");
+    var contact_item = document.getElementById("contact-item");
+    var breadcrumbs = document.getElementById("breadcrumbs");
+    breadcrumbs.innerText = "/contacts"
     about.classList.add("hide");
     contact.classList.remove("hide");
+    contact_item.classList.add("active");
 }
 
 function toHome() {
     var about = document.getElementById("about");
     var contact = document.getElementById("contact");
+    var contact_item = document.getElementById("contact-item");
+    var breadcrumbs = document.getElementById("breadcrumbs");
+    breadcrumbs.innerText = ""
     about.classList.remove("hide");
     contact.classList.add("hide");
+    contact_item.classList.remove("active");
+}
+
+
+function toMobContact() {
+    console.log("hello");
+    var mob_contact_item = document.getElementById("mob-contact-item");
+    var mob_contact = document.getElementById("mob-contact");
+    var mob_about = document.getElementById("mob-about");
+    var mob_projects = document.getElementById("mob-projects");
+    mob_about.classList.add("hide");
+    mob_projects.classList.add("hide");
+    mob_contact.classList.remove("hide");
+    mob_contact_item.classList.add("active")
 }
